94296d73ffe7bca19950352f08d0e1d3155dcac7
Adrian-sosaa <a.22308051281317@cbtis128.edu.mx>
2024-09-06 09:33:15 -0600

    Subi dos archivos

---
+++ miscadenas.py
@@ -0,0 +1,7 @@
+nombre="Adrian"
+print(nombre[0])
+print(nombre)
+
+MisNovias=["Agripina","Anastacia","Maria"]
+for chicas in MisNovias:
+    print(chicas)
---
+++ mivariables.py
@@ -0,0 +1,18 @@
+import math
+edad=17  #&% se guarda en edad
+
+nombre= "Jan sosa"
+print("tu edad es",edad)
+print("te llamas",nombre)
+print("-- tipo de variables ---")
+x = str(7)    # x will be '3'
+y = int(3.1416)    # y will be 3
+z=float(7)
+print(x)
+print(y)
+print(z)
+print("-- Funciones matematicas ---")
+x = math.sqrt(64)
+print(x)
+
+
